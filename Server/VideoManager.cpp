b8fb4173ebdedace6617a9f3c610c637917eb277
naumtsev <naumtsevanton@mail.ru>
2021-05-28 21:23:06 +0300

    add clang format to server

--- Server/VideoManager.cpp
+++ Server/VideoManager.cpp
@@ -1,73 +1,74 @@
 #include "VideoManager.h"
 
 VideoManager::VideoManager(Server *server_) : QObject(), server(server_) {
-    current_video.state = Empty;
+  current_video.state = Empty;
 }
 
-
-Video VideoManager::get_current_video(){
-       if(current_video.state == Empty) {
-            return current_video;
-       } else if(current_video.state == Playing) {
-             current_video.progress += QDateTime::currentMSecsSinceEpoch() - last_time_pause;
-             last_time_pause = QDateTime::currentMSecsSinceEpoch();
-             return current_video;
-       } else if(current_video.state == Pause) {
-            return current_video;
-       }
-}
-
-void VideoManager::set_new_video(const QJsonObject &json_video){
-    Video video(json_video);
-    video.begin_time = QDateTime::currentMSecsSinceEpoch();
+Video VideoManager::get_current_video() {
+  if (current_video.state == Empty) {
+    return current_video;
+  } else if (current_video.state == Playing) {
+    current_video.progress +=
+        QDateTime::currentMSecsSinceEpoch() - last_time_pause;
     last_time_pause = QDateTime::currentMSecsSinceEpoch();
-    video.progress = 0;
-    video.state = Playing;
-    current_video = video;
-    QJsonObject req;
-    req.insert("type", "video_event");
-    req.insert("event_type", "set_video");
-    req.insert("video", video.to_json());
-    QJsonDocument doc(req);
+    return current_video;
+  } else if (current_video.state == Pause) {
+    return current_video;
+  }
+}
 
-    server->send_data_to_all_users(doc.toJson());
+void VideoManager::set_new_video(const QJsonObject &json_video) {
+  Video video(json_video);
+  video.begin_time = QDateTime::currentMSecsSinceEpoch();
+  last_time_pause = QDateTime::currentMSecsSinceEpoch();
+  video.progress = 0;
+  video.state = Playing;
+  current_video = video;
+  QJsonObject req;
+  req.insert("type", "video_event");
+  req.insert("event_type", "set_video");
+  req.insert("video", video.to_json());
+  QJsonDocument doc(req);
+
+  server->send_data_to_all_users(doc.toJson());
 }
 
 void VideoManager::pause_video() {
-    if(current_video.state == Playing) {
-        current_video.progress += QDateTime::currentMSecsSinceEpoch() - last_time_pause;
-        last_time_pause = QDateTime::currentMSecsSinceEpoch();
-        current_video.state = Pause;
-
-        QJsonObject req;
-        req.insert("type", "video_event");
-        req.insert("event_type", "pause");
-        req.insert("video", current_video.to_json());
-
-        QJsonDocument doc(req);
-        server->send_data_to_all_users(doc.toJson());
-        return;
-    } else if(current_video.state == Pause) {
-        last_time_pause = QDateTime::currentMSecsSinceEpoch();
-        current_video.state = Playing;
-        QJsonObject req;
-        req.insert("type", "video_event");
-        req.insert("event_type", "pause");
-        req.insert("video", current_video.to_json());
-
-        QJsonDocument doc(req);
-        server->send_data_to_all_users(doc.toJson());
-        return;
-    }
-}
-
-void VideoManager::stop_video() {
-    current_video = Video();
+  if (current_video.state == Playing) {
+    current_video.progress +=
+        QDateTime::currentMSecsSinceEpoch() - last_time_pause;
+    last_time_pause = QDateTime::currentMSecsSinceEpoch();
+    current_video.state = Pause;
 
     QJsonObject req;
     req.insert("type", "video_event");
-    req.insert("event_type", "stop");
+    req.insert("event_type", "pause");
+    req.insert("video", current_video.to_json());
 
     QJsonDocument doc(req);
     server->send_data_to_all_users(doc.toJson());
+    return;
+  } else if (current_video.state == Pause) {
+    last_time_pause = QDateTime::currentMSecsSinceEpoch();
+    current_video.state = Playing;
+    QJsonObject req;
+    req.insert("type", "video_event");
+    req.insert("event_type", "pause");
+    req.insert("video", current_video.to_json());
+
+    QJsonDocument doc(req);
+    server->send_data_to_all_users(doc.toJson());
+    return;
+  }
+}
+
+void VideoManager::stop_video() {
+  current_video = Video();
+
+  QJsonObject req;
+  req.insert("type", "video_event");
+  req.insert("event_type", "stop");
+
+  QJsonDocument doc(req);
+  server->send_data_to_all_users(doc.toJson());
 }
